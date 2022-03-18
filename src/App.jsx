4818d95a4f2c08d4c9349681df86c51dbb3136ab
Tony Jemba <jjembatony@gmail.com>
2022-03-18 16:56:14 +0300

    adding react ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,62 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import { render } from "react-dom";
+import "antd/dist/antd.css";
+import Form from "./voteForm";
+import { Layout, Typography } from "antd";
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const MyHello = () => {
+  const { Header, Content, Footer } = Layout;
+  const { Title } = Typography;
+
+
+  
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
-}
+    <div>
+      <Layout className="layout">
+        <Header>
+          <h2 style={{ color: "white" }}>Voting on the internet computer</h2>
+        </Header>
+        <Content style={{ padding: "0 100px", height: "80vh" }}>
+          <div style={{ marginTop: "40px" }} className="site-layout-content">
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-around",
+                width: "100%",
+                marginTop: "40px",
+              }}
+            >
+              <div>
+                <Form color="red" name={"Candidate A"} votes={canA} />
+              </div>
 
-export default App
+              <div>
+                <Form color="blue" name={"Candidate B"} votes={candidateB} />
+              </div>
+            </div>
+
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                width: "100%",
+                marginTop: "40px",
+              }}
+            >
+              <Title level={3}>Lead:</Title>
+            </div>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: "center" }}>
+          Voting dApp Motoko Bootcamp ©2022 Created by Jemba Tony
+        </Footer>
+      </Layout>
+    </div>
+  );
+};
+
+render(<MyHello />, document.getElementById("app"));
